13d537dadd8678e576103e2c3cff96ae36aae18c
DSdatsme <darsuratwala@gmail.com>
2020-04-05 15:39:04 +0530

    first commit

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=dsdatsme/scikit-house-prediction
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker tag flask-model $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
